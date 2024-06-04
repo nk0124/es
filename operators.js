6f373a9c5fa8ae9ab0a14a54746de708b3ea8dd2
nk0124 <160753539+nk0124@users.noreply.github.com>
2024-06-04 11:56:46 +0530

    Update operators.js

--- operators.js
+++ operators.js
@@ -1,12 +1,11 @@
-// spread ko calling time use kerte hai iski satring hoti hai ... ke sath let suppose two array hai uske elements ko milaker ek array 
-// bnaya hai spread op. ka use hum two array ke elements ko open or spread kerne ke kerte hai 
+
 const average=(a,b,c)=>(a+b+c)/3
 console.log(average(45,67,88))
 
 let nums =[48,67,82]
 // console.log(average(num[0],num[1],num[2]))
-console.log(average(...nums)) // spread
+console.log(average(...nums)) 
 
 let mynums=[60,89,43];
-mynums.push(...nums) // spread
+mynums.push(...nums) 
 console.log(mynums)
