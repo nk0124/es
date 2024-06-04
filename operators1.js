f6de142595e4b8cec8848b2b18bd57e148fd12d4
nk0124 <160753539+nk0124@users.noreply.github.com>
2024-06-04 11:57:16 +0530

    Update operators1.js

--- operators1.js
+++ operators1.js
@@ -1,6 +1,4 @@
-// rest opr
-// rest opr ko hum define hum arrow fun. ke arguments mai kerte hai .running time pe hum arrguments pass kerte hai 
-// vo baad mai rest opr ki wajh se ek array create ho jata hai .
+
 let total=(...nums)=>{
     console.log(nums)
     let s = 0;
